8585b1b14769ab6ef65ff77bf76ebe7eaafa52a7
csillaboglarka <bendecsilla@yahoo.com>
2019-10-02 14:11:01 +0300

    lastname

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final String LAST_NAME = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+    public void goProfile(View view) {
+
+        Intent profintent = new Intent(this, Profile.class);
+        EditText lastNname = (EditText) findViewById(R.id.lastName);
+        String last_name = lastNname.getText().toString();
+        profintent.putExtra("LAST_NAME", last_name);
+        startActivity(profintent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+        String last_name = intent.getStringExtra("LAST_NAME");
+        TextView lastName = findViewById(R.id.lastName);
+        lastName.setText(last_name);
+    }
+
+
+
+}
